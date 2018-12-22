e8e8958548120dd41c017711bd5280345a277893
Carlos Nah <cnah27@gmail.com>
2018-12-22 00:10:48 +0000

    Automatically backed up by Learn

--- other_file.js
+++ other_file.js
@@ -1,7 +1,2 @@
 var index = require("./index.js");
-
-if (name === "Susan") {
-    console.log("The name is correct")
-}else {
-    console.log("Expected the name to be Susan")
-}
+console.log(index.name)
